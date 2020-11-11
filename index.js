aef68c983efd981e1f2619ca42ec85bbe9949e9d
Kunika <38423159+kunikap@users.noreply.github.com>
2020-11-11 19:14:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const lastOccurance = (arr) => {
+  return arr.filter((item, currentIndex) => {
+    return arr.lastIndexOf(item) === currentIndex;
+  })
+
+}
+
+console.log(lastOccurance([7, 2, 4, 8, 2, 7, 4])); //[8,2,7,4]
