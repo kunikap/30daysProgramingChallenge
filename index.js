67849d2a1291e6008c3b4532c18e76a1384aee99
Kunika <38423159+kunikap@users.noreply.github.com>
2020-11-11 19:38:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const lastOccurance = (arr) => {
 }
 
 console.log(lastOccurance([7, 2, 4, 8, 2, 7, 4])); //[8,2,7,4]
+
+
+// find sum of two elements which is equal to the given target
+const findSumOfElement = (arr, target) => {
+  let myMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (myMap.has(complement)) {
+      return [myMap.get(complement), i];
+    }
+    myMap.set(arr[i], i)
+  }
+
+}
+
+console.log(findSumOfElement([2, 5, 7, 9], 9)); // [0,2]
+
