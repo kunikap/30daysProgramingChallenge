afcb46456b72f92fc30d11a978b20d898be0a564
Kunika <38423159+kunikap@users.noreply.github.com>
2020-11-11 19:20:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
+//find the last occurances of elements in an array mainitaining the same order in which they occur
 const lastOccurance = (arr) => {
-  return arr.filter((item, currentIndex) => {
-    return arr.lastIndexOf(item) === currentIndex;
-  })
-
+  return arr.filter((item, currentIndex) => arr.lastIndexOf(item) === currentIndex)
 }
 
 console.log(lastOccurance([7, 2, 4, 8, 2, 7, 4])); //[8,2,7,4]
