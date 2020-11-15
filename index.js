f29abac11a43a86b801aaeb271086c8f21cf804d
Kunika <38423159+kunikap@users.noreply.github.com>
2020-11-15 14:57:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,26 @@ const findSumOfElement = (arr, target) => {
 
 console.log(findSumOfElement([2, 5, 7, 9], 9)); // [0,2]
 
+
+//find subArrays which satiesfies given conditions of maxSum and max number of elements
+// arr = [10, 100, 20, 50, 20, 50, 70, 120, 90] , maxSum = 150, maxlen = 3
+// output = [[10, 100, 20], [50, 20, 50], [70], [120]]
+
+function formattedArray(arr, maxSum, maxNumberOfElements) {
+  let currentSum = 0;
+  let result = [];
+  let currentArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const currentElement = arr[i];
+    if (currentSum + currentElement > maxSum || currentArray.length === maxNumberOfElements) {
+      result.push(currentArray);
+      currentArray = [];
+      currentSum = 0
+    }
+    currentArray.push(currentElement);
+    currentSum = +currentElement
+  }
+  result.push(currentArray);
+  return result;
+
+}
