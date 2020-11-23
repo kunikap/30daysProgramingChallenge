451363b5f6459a9d7b1cbb0770c7fd3a3a92d074
Kunika <38423159+kunikap@users.noreply.github.com>
2020-11-23 15:28:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,3 +45,17 @@ function formattedArray(arr, maxSum, maxNumberOfElements) {
   return result;
 
 }
+//insertion sort
+function insertionSort(arr){
+    for(let i = 1; i< arr.length; i++){
+        let current = arr[i]
+        let j = i-1;
+while((j>-1) && (arr[j]>current)){
+        arr[j+1] = arr[j]
+            j-- ;
+        }
+            arr[j+1] = current;
+    }
+        return arr;    
+}
+console.log(insertionSort([23,67,96,34,55])); //(5) [23, 34, 55, 67, 96
